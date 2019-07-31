9798759d5255428027b4b6502bda99776a9f3283
Ayesiza Hawah <yesiha2001@gmail.com>
2019-07-31 12:12:29 -0700

    feature(signin):user login into account [finishes#167593803]

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -506,4 +506,43 @@ it('validate required address', (done) => {
         done();
       });
   });
+  it('signIn/login success', (done) => {
+    request(app)
+      .post('/api/v1/users/auth/signin')
+      .send({ 
+        "email": "jane@gmail.com",
+        "password": "jane5555"
+        })
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.body.message.should.equal('User is successfully logged in');
+        done();
+      });
+  });
+  it('signIn is user not exist', (done) => {
+    request(app)
+      .post('/api/v1/users/auth/signin')
+      .send({ 
+        "email": "keneth@gmail.com",
+        "password": "keneth5555"
+        })
+      .end((err, res) => {
+        res.status.should.equal(404);
+        res.body.message.should.equal('user not found');
+        done();
+      });
+  });
+  it('signIn on wrong details', (done) => {
+    request(app)
+      .post('/api/v1/users/auth/signin')
+      .send({ 
+        "email": "jane@gmail.com",
+        "password": "jan5668"
+        })
+      .end((err, res) => {
+        res.status.should.equal(400);
+        res.body.message.should.equal('wrong email or password');
+        done();
+      });
+  });
 });
