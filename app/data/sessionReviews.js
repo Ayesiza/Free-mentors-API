1f6e0c7fd4d4709130dbba534a42ae98b4eebdf8
Ayesiza Hawah <yesiha2001@gmail.com>
2019-09-08 12:51:24 -0700

    feature(accept-session):Mentor can accept mentorship session [finishes#168358945]

--- app/data/sessionReviews.js
+++ app/data/sessionReviews.js
@@ -1,11 +1,11 @@
 
-export const sessionReviews = [
-    {
-        sessionId: 1,
-        mentorId: 2,
-        menteeId: 2,
-        score: 5,
-        menteeFullName: 'sherifa mogan',
-        remark: 'very good',
-    }
-]
+// export const sessionReviews = [
+//     {
+//         sessionId: 1,
+//         mentorId: 2,
+//         menteeId: 2,
+//         score: 5,
+//         menteeFullName: 'sherifa mogan',
+//         remark: 'very good',
+//     }
+// ]
